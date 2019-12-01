e083f1d0807a9116124ff22b3c72e88a8cad8ec5
Daniel Rodrigues <daniel.rodrighes@gmail.com>
2019-12-01 00:13:47 -0300

    adicionado docker

--- configurekubernetes.sh
+++ configurekubernetes.sh
@@ -13,7 +13,7 @@ EOF
 setenforce 0
 sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 
-yum install -y  podman
+yum install -y  docker podman
 yum install -y  kubelet kubeadm kubectl --disableexcludes=kubernetes
 
 systemctl enable --now kubelet
