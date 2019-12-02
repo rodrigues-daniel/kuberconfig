8145064c2768b87623da6950e69de223df9b665d
Daniel Rodrigues <daniel.rodrighes@gmail.com>
2019-12-01 22:51:48 -0300

    Update minikube_install.sh

--- minikube_install.sh
+++ minikube_install.sh
@@ -1,1 +1,5 @@
 
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-1.5.2.rpm  && sudo rpm -ivh minikube-1.5.2.rpm
+ 
+sudo minikube start --vm-driver=none
+sudo minikube config set vm-driver none
