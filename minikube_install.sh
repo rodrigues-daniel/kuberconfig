fa2af05cf14f901fc9869f5b18b315acc3c39995
Daniel Cosme Rodrigues <daniel@apofis.localdomain>
2019-12-01 23:12:55 -0300

    base

--- minikube_install.sh
+++ minikube_install.sh
@@ -1,5 +1,14 @@
 
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-1.5.2.rpm  && sudo rpm -ivh minikube-1.5.2.rpm
  
+systemctl stop firewald
+systemctl disable firewald
+yum install -y iptables-services
+systemctl enable iptables.service
+systemctl start iptables.service
+yum install -y system-config-firewall-tui.noarch
+system-config-firewall-tui
+
+sudo swapoff -a
 sudo minikube start --vm-driver=none
 sudo minikube config set vm-driver none
