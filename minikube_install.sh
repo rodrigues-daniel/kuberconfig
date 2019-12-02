c724f4df59fd132b860a908fd1aabc3fbc584c31
Daniel Rodrigues <daniel.rodrighes@gmail.com>
2019-12-02 00:34:59 -0300

    Update minikube_install.sh

--- minikube_install.sh
+++ minikube_install.sh
@@ -1,6 +1,9 @@
 
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64  && chmod +x minikube
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64  &&  chmod +x minikube
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
 
+sudo mv ./kubectl /usr/local/bin/kubectl
 sudo mkdir -p /usr/local/bin/
 sudo install minikube /usr/local/bin/
  
